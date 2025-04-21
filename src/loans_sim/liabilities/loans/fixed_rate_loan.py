6880a06ac880edd958f8ccbe89f880edc1fc5bbe
zackjyoung3 <zackjyoung3@gmail.com>
2025-04-21 12:59:05 -0400

    Refactor for an OOP approach to Loan operations

--- src/loans_sim/liabilities/loans/fixed_rate_loan.py
+++ src/loans_sim/liabilities/loans/fixed_rate_loan.py
@@ -1,10 +1,55 @@
+from collections import OrderedDict
+from dataclasses import dataclass
 from decimal import Decimal
+from typing import Self
 
+import polars as pl
 from pydantic import BaseModel, constr, computed_field
 
 import loans_sim.constants as C
 from loans_sim.custom_pydantic.annotations import DollarDecimal
 from loans_sim.utils import get_monthly_interest_rate, round_dollar_to_nearest_cent
+
+
+PAYMENT_PLAN_SCHEMA = OrderedDict(
+    [
+        ("Month", pl.Int64),
+        ("Total Remaining", pl.Decimal(scale=2)),
+        ("Principal Remaining", pl.Decimal(scale=2)),
+        ("Interest Remaining", pl.Decimal(scale=2)),
+        ("Cum Total Paid", pl.Decimal(scale=2)),
+        ("Monthly Principal Paid", pl.Decimal(scale=2)),
+        ("Monthly Interest Paid", pl.Decimal(scale=2)),
+    ]
+)
+
+
+class PaymentInfo(BaseModel):
+    interest_paid: DollarDecimal
+    principal_paid: DollarDecimal
+
+    @computed_field
+    @property
+    def total_paid(self) -> Decimal:
+        return self.interest_paid + self.principal_paid
+
+
+@dataclass(frozen=True)
+class PaymentRes:
+    loan_status: "FixedRateLoan"  # forward ref to FixedRateLoan
+    payment_info: PaymentInfo
+
+
+def _make_payment_plan_info(loan: "FixedRateLoan", payment: PaymentInfo, month_no: int) -> dict:
+    return {
+        "Month": month_no,
+        "Total Remaining": loan.current_amount,
+        "Principal Remaining": loan.principal,
+        "Interest Remaining": loan.interest,
+        "Cum Total Paid": loan.lifetime_payments,
+        "Monthly Principal Paid": payment.principal_paid,
+        "Monthly Interest Paid": payment.interest_paid,
+    }
 
 
 class FixedRateLoan(BaseModel):
@@ -31,3 +76,81 @@ class FixedRateLoan(BaseModel):
     @property
     def is_paid_off(self) -> bool:
         return self.current_amount == C.ZERO_DOLLARS_DECIMAL
+
+    def after_monthly_interest_accum(self) -> Self:
+        """New instance of the loan after interest is accumulated for a month"""
+        month_accum_interest = self.principal * Decimal(str(self.monthly_interest_rate))
+        month_accum_interest = round_dollar_to_nearest_cent(month_accum_interest)
+        current_amount_w_interest = self.current_amount + month_accum_interest
+
+        return self.model_copy(update={"current_amount": current_amount_w_interest})
+
+    def make_payment(self, payment: Decimal) -> PaymentRes:
+        payment = round_dollar_to_nearest_cent(payment)
+        possible_debit_for_principal = -(self.interest - payment)
+        if possible_debit_for_principal > 0:
+            interest_paid = self.interest
+            principal_paid = min(possible_debit_for_principal, self.principal)
+        else:
+            interest_paid = payment
+            principal_paid = C.ZERO_DOLLARS_DECIMAL
+        payment_info = PaymentInfo(interest_paid=interest_paid, principal_paid=principal_paid)
+        new_loan_state = self.model_copy(
+            update={
+                "current_amount": self.current_amount - payment_info.total_paid,
+                "principal": self.principal - payment_info.principal_paid,
+                "lifetime_payments": self.lifetime_payments + payment_info.total_paid,
+            }
+        )
+
+        return PaymentRes(loan_status=new_loan_state, payment_info=payment_info)
+
+    def make_monthly_payment(self) -> PaymentRes:
+        """
+        A monthly payment is a 2 step process in which
+        1. The interest from the prev month is accumulated
+        2. The loans monthly payment is applied to the loan with that accum interest
+        """
+        self_with_monthly_interest = self.after_monthly_interest_accum()
+        return self_with_monthly_interest.make_payment(self.monthly_payment)
+
+    def compute_payment_plan(self) -> pl.DataFrame:
+        """
+        Generate a polars DF with all information for the Payment plan of the form
+
+        Note that this strictly adheres to the monthly payment plan that has been provided
+        I have experimented with online tools that generate amortized payment plans and there will be very slight deviation
+        (generally few cents on $10,000 basis) that result from the payment plans generated having some months where they
+        state the payment as being a certain amount, but then have monthly payment exceed this payment by a cent.
+        This implementation adheres absolute fidelity to the stated monthly payment plan.
+        :param loan: the state of the loans that is to be paid off
+        :return: a DataFrame with monthly payment plan information of the form
+        ┌───────┬───────────────┬──────────────┬──────────────┬──────────────┬──────────────┬──────────────┐
+        │ Month ┆ Total         ┆ Principal    ┆ Interest     ┆ Cum Total    ┆ Monthly      ┆ Monthly      │
+        │       ┆ Remaining     ┆ Remaining    ┆ Remaining    ┆ Paid         ┆ Principal    ┆ Interest     │
+        │  ---  ┆ ---           ┆ ---          ┆ ---          ┆ ---          ┆ Paid         ┆ Paid         │
+        ╞═══════╪═══════════════╪══════════════╪══════════════╪══════════════╪══════════════╪══════════════╡
+        """
+        loan = self
+        month = 1
+        states = []
+
+        while not loan.is_paid_off:
+            payment_res = loan.make_monthly_payment()
+            loan, payment_made = payment_res.loan_status, payment_res.payment_info
+            states.append(_make_payment_plan_info(loan, payment_made, month))
+            month += 1
+
+        return pl.DataFrame(states, schema=PAYMENT_PLAN_SCHEMA)
+
+    # use method rather than property for this as it is non-trivial computation
+    def get_total_payment_req(self) -> Decimal:
+        """
+        Less efficient than amortized calculation obviously, but permits arbitrary start date calculations e.g.
+        we resolve lifetime payment for a loan at any arbitrary state in terms of principal and interest rather than
+        requiring that we just have initial amount (principal)
+        """
+        payment_plan = self.compute_payment_plan()
+        if payment_plan.is_empty():
+            return C.ZERO_DOLLARS_DECIMAL
+        return payment_plan.select(pl.last("Cum Total Paid")).item()
--- test/unit/liabilities/loans/test_fixed_rate_loan.py
+++ test/unit/liabilities/loans/test_fixed_rate_loan.py
@@ -1,6 +1,17 @@
+# TODO: come back and refactor tests
+#   I initially had more procedural approach to making monthly payments, but wanted to encapsulate
+#   all logic for performing operations on the loan in a class for a more OOP approach
+#   should come back and make explicit make_payment tests (being tested implicitly via make_monthly_payment
+#   which would have all cases)
+import copy
 from decimal import Decimal
 
-from loans_sim.liabilities.loans.fixed_rate_loan import FixedRateLoan
+import polars as pl
+import polars.testing as plt
+import pytest
+
+import loans_sim.constants as C
+from loans_sim.liabilities.loans.fixed_rate_loan import FixedRateLoan, PAYMENT_PLAN_SCHEMA
 
 
 def test_interest_computed_correctly():
@@ -48,3 +59,217 @@ def test_is_paid_off_true():
         monthly_payment=Decimal("50.00"),
     )
     assert loan.is_paid_off
+
+
+# ensure total_paid updated correctly for from scratch + running total cases
+@pytest.mark.parametrize("total_paid", [0, 200], ids=["first_payment", "subsequent_payment"])
+def test_after_monthly_payment_payoff_less_than_accum_interest_plus_current_amount(total_paid: int):
+    loan_in_state_to_be_paid_off_in_a_month = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("150.00"),
+        lifetime_payments=total_paid,
+    )
+    expected_paid = Decimal("131.00")  # 130 balance + (1% monthly * 100 principal)
+    original = copy.deepcopy(loan_in_state_to_be_paid_off_in_a_month)
+
+    after_payment_res = loan_in_state_to_be_paid_off_in_a_month.make_monthly_payment()
+    after_payment = after_payment_res.loan_status
+    payment_info = after_payment_res.payment_info
+
+    assert loan_in_state_to_be_paid_off_in_a_month == original  # no side effects
+    assert after_payment is not original  # new copy returned
+
+    assert after_payment.current_amount == C.ZERO_DOLLARS_DECIMAL
+    assert after_payment.principal == C.ZERO_DOLLARS_DECIMAL
+    assert after_payment.lifetime_payments == Decimal(str(total_paid + expected_paid))
+    assert after_payment.is_paid_off is True
+
+    assert payment_info.interest_paid == 31
+    assert payment_info.principal_paid == 100
+
+
+# ensure total_paid updated correctly for from scratch + running total cases
+@pytest.mark.parametrize("total_paid", [0, 200], ids=["first_payment", "subsequent_payment"])
+def test_after_monthly_payment_only_interest_can_be_paid_off(total_paid: int):
+    loan_only_interest_can_be_paid = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("30.99"),
+        lifetime_payments=total_paid,
+    )
+    expected_principal = loan_only_interest_can_be_paid.principal
+    expected_paid = Decimal("30.99")  # full monthly payment expected
+    expected_interest = Decimal("0.01")
+    expected_total = expected_principal + expected_interest
+    original = copy.deepcopy(loan_only_interest_can_be_paid)
+
+    after_payment_res = loan_only_interest_can_be_paid.make_monthly_payment()
+    after_payment = after_payment_res.loan_status
+    payment_info = after_payment_res.payment_info
+
+    assert loan_only_interest_can_be_paid == original  # no side effects
+    assert after_payment is not original  # new copy returned
+
+    assert after_payment.current_amount == expected_total
+    assert after_payment.principal == expected_principal
+    assert after_payment.interest == expected_interest
+    assert after_payment.lifetime_payments == Decimal(str(total_paid + expected_paid))
+    assert after_payment.is_paid_off is False
+
+    assert payment_info.interest_paid == expected_paid
+    assert payment_info.principal_paid == Decimal("0")
+
+
+# ensure total_paid updated correctly for from scratch + running total cases
+@pytest.mark.parametrize("total_paid", [0, 200], ids=["first_payment", "subsequent_payment"])
+def test_after_monthly_payment_only_interest_can_be_paid_off_boundary(total_paid: int):
+    loan_only_interest_can_be_paid = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("31"),
+        lifetime_payments=total_paid,
+    )
+    expected_principal = loan_only_interest_can_be_paid.principal
+    expected_paid = Decimal("31")  # full monthly payment expected
+    expected_interest = C.ZERO_DOLLARS_DECIMAL
+    expected_total = expected_principal + expected_interest
+    original = copy.deepcopy(loan_only_interest_can_be_paid)
+
+    after_payment_res = loan_only_interest_can_be_paid.make_monthly_payment()
+    after_payment = after_payment_res.loan_status
+    payment_info = after_payment_res.payment_info
+
+    assert loan_only_interest_can_be_paid == original  # no side effects
+    assert after_payment is not original  # new copy returned
+
+    assert after_payment.current_amount == expected_total
+    assert after_payment.principal == expected_principal
+    assert after_payment.interest == expected_interest
+    assert after_payment.lifetime_payments == Decimal(str(total_paid + expected_paid))
+    assert after_payment.is_paid_off is False
+
+    assert payment_info.interest_paid == expected_paid
+    assert payment_info.principal_paid == Decimal("0")
+
+
+# ensure total_paid updated correctly for from scratch + running total cases
+@pytest.mark.parametrize("total_paid", [0, 200], ids=["first_payment", "subsequent_payment"])
+def test_after_monthly_payment_principal_can_be_paid_off(total_paid: int):
+    loan_principal_can_be_paid_after_interest_paid = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("50"),
+        lifetime_payments=total_paid,
+    )
+    # 31 in interest paid off => 19 left over for principal
+    expected_principal = loan_principal_can_be_paid_after_interest_paid.principal - Decimal("19.00")
+    expected_paid = Decimal("50")  # full monthly payment expected
+    expected_interest = C.ZERO_DOLLARS_DECIMAL
+    expected_total = expected_principal + expected_interest
+    original = copy.deepcopy(loan_principal_can_be_paid_after_interest_paid)
+
+    after_payment_res = loan_principal_can_be_paid_after_interest_paid.make_monthly_payment()
+    after_payment = after_payment_res.loan_status
+    payment_info = after_payment_res.payment_info
+
+    assert loan_principal_can_be_paid_after_interest_paid == original  # no side effects
+    assert after_payment is not original  # new copy returned
+
+    assert after_payment.current_amount == expected_total
+    assert after_payment.principal == expected_principal
+    assert after_payment.interest == expected_interest
+    assert after_payment.lifetime_payments == Decimal(str(total_paid + expected_paid))
+    assert after_payment.is_paid_off is False
+
+    assert payment_info.interest_paid == Decimal("31")
+    assert payment_info.principal_paid == Decimal("19")
+
+
+def test_compute_payment_plan_loan_already_paid_off_empty():
+    loan_in_state_to_be_paid_off_in_a_month = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("0.00"),
+        principal=Decimal("0.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("150.00"),
+    )
+
+    payment_plan = loan_in_state_to_be_paid_off_in_a_month.compute_payment_plan()
+
+    assert payment_plan.is_empty()
+    assert payment_plan.schema == PAYMENT_PLAN_SCHEMA
+
+
+def test_compute_payment_plan_one_month_payoff():
+    loan_in_state_to_be_paid_off_in_a_month = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("150.00"),
+    )
+    expected_single_month_info = {
+        "Month": 1,
+        "Total Remaining": Decimal("0.00"),
+        "Principal Remaining": Decimal("0.00"),
+        "Interest Remaining": Decimal("0.00"),
+        "Cum Total Paid": Decimal("131.00"),
+        "Monthly Principal Paid": Decimal("100.00"),
+        "Monthly Interest Paid": Decimal("31.00"),
+    }
+    expected_df = pl.DataFrame(expected_single_month_info, schema=PAYMENT_PLAN_SCHEMA)
+    payment_plan = loan_in_state_to_be_paid_off_in_a_month.compute_payment_plan()
+
+    assert payment_plan.height == 1  # single payment req
+    assert payment_plan.schema == PAYMENT_PLAN_SCHEMA
+    plt.assert_frame_equal(payment_plan, expected_df)
+
+
+def test_get_total_payment_req_no_payoff_case():
+    loan_in_state_to_be_paid_off_in_a_month = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("0.00"),
+        principal=Decimal("0.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("150.00"),
+    )
+
+    assert loan_in_state_to_be_paid_off_in_a_month.get_total_payment_req() == C.ZERO_DOLLARS_DECIMAL
+
+
+def test_get_total_payment_req_one_month_payoff():
+    loan_in_state_to_be_paid_off_in_a_month = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("150.00"),
+    )
+    # 130 existing amount + 1 for interest accumulated in that month
+
+    assert loan_in_state_to_be_paid_off_in_a_month.get_total_payment_req() == Decimal("131.00")
+
+
+def test_get_total_payment_req_n_month_payoff():
+    loan_in_state_to_be_paid_off_in_n_months = FixedRateLoan(
+        vendor="TestBank",
+        current_amount=Decimal("130.00"),
+        principal=Decimal("100.00"),
+        annual_interest_rate=0.12,
+        monthly_payment=Decimal("50.00"),
+    )
+    # 50 first month off 131 => 81 remaining (50 paid)
+    # 81 => .81 interest => 31.81 remaining (50 paid)
+    # 31.81 => .32 interest => no remaining (32.13 paid)
+    # 50 + 50 + 32.13 = 132.13 total
+
+    assert loan_in_state_to_be_paid_off_in_n_months.get_total_payment_req() == Decimal("132.13")
