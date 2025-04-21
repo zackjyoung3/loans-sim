ab066a60587a171c8a491d484e2a4d5255ab68c7
zackjyoung3 <zackjyoung3@gmail.com>
2025-04-21 14:28:33 -0400

    Simple High Yields Savings account impl

---
+++ src/loans_sim/assets/savings_account/high_yield.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from typing import Self
+
+from pydantic import BaseModel, computed_field, constr
+
+import loans_sim.constants as C
+from loans_sim.custom_pydantic.annotations import DollarDecimal
+from loans_sim.utils import get_monthly_rate, round_dollar_to_nearest_cent
+
+
+class HighYieldSavingsAccount(BaseModel):
+    vendor: constr(min_length=1)
+    apy: float
+    balance: DollarDecimal = C.ZERO_DOLLARS_DECIMAL
+
+    @computed_field
+    @property
+    def average_monthly_yield(self) -> float:
+        return get_monthly_rate(self.apy)
+
+    def after_one_month(self) -> Self:
+        new_balance = self.balance + round_dollar_to_nearest_cent(
+            self.balance * Decimal(str(self.average_monthly_yield))
+        )
+        return self.model_copy(update={"balance": new_balance})
---
+++ test/unit/assets/savings_account/high_yield.py
@@ -0,0 +1,34 @@
+from decimal import Decimal
+
+import loans_sim.constants as C
+from loans_sim.assets.savings_account.high_yield import HighYieldSavingsAccount
+
+
+def test_average_monthly_yield():
+    savings_account = HighYieldSavingsAccount(
+        vendor="test",
+        apy=0.12,
+    )
+
+    assert savings_account.average_monthly_yield == 0.01
+
+
+def test_high_yield_savings_account_advanced_after_one_month_zero_stays_zero():
+    savings_account = HighYieldSavingsAccount(
+        vendor="test",
+        apy=0.12,
+    )
+
+    after_one_month = savings_account.after_one_month()
+
+    assert after_one_month is not savings_account  # new instance
+    assert after_one_month.balance == C.ZERO_DOLLARS_DECIMAL
+
+
+def test_high_yield_savings_account_advanced_after_one_month_simple_one_percent():
+    savings_account = HighYieldSavingsAccount(vendor="test", apy=0.12, balance=Decimal("100.00"))
+
+    after_one_month = savings_account.after_one_month()
+
+    assert after_one_month is not savings_account  # new instance
+    assert after_one_month.balance == Decimal("101.00")
